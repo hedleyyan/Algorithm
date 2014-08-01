ce110ab2cf365b353308e909287b14ca300f1bae
hedley <hedleyyan@gmail.com>
2014-08-01 18:35:44 +0800

    stack

---
+++ src/collection/Stack.java
@@ -0,0 +1,8 @@
+package collection;
+
+public interface Stack<Item> {
+    Item pop();
+    Item peek();
+    void push(Item item);
+    boolean isEmpty();
+}   
---
+++ src/collection/StackAdapter.java
@@ -0,0 +1,30 @@
+package collection;
+
+public class StackAdapter<Item> implements Stack<Item> {
+    private java.util.Stack<Item> stack;
+    
+    public StackAdapter() {
+        stack = new java.util.Stack<Item>(); 
+    }
+    
+    @Override
+    public Item pop() {
+        return stack.pop();
+    }
+
+    @Override
+    public void push(Item item) {
+        stack.push(item);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    @Override
+    public Item peek() {
+        return stack.peek();
+    }
+
+}
